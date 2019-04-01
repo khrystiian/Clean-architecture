62e85b2914470efa86af56e751d4c8af78bbe631
khrystiian <a.ciobanu19@gmail.com>
2019-04-01 13:36:16 +0200

    refactoring

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -12,7 +12,7 @@ namespace UI
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
-            app.MapSignalR();
+            //app.MapSignalR();
         }
     }
 }
